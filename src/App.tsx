0aa01be6ae5a2fc3e8c289b874263d3d05dc9096
Bryan Jeff Saycon <bryanjeff.saycon@g.msuiit.edu.ph>
2022-03-26 01:38:05 +0800

    finally! working code but still messy code bad code practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,77 @@
-import {useState} from 'react';
-import { InputForm } from './InputForm';
-import './styles/App.css';
-import { Welcome } from './Welcome';
-import { useFetch } from './useFetch';
-import { OutputCard } from './OutputCard';
+import { useState } from "react";
+import { InputForm } from "./InputForm";
+import "./styles/App.css";
+import { Welcome } from "./Welcome";
+import { useFetch } from "./useFetch";
+import { OutputCard } from "./OutputCard";
 
 const App = () => {
-  const [seed, setSeed] = useState('');
-  const url:string = `https://randomuser.me/api/?seed=${seed}`; //seed value is from inputbox
-  const data = useFetch(url); //the url relies on seed input
+  interface UserData {
+      results?: Array<Results>;
+      info?: unknown;
+  }
+  interface Results {
+    name?: Name;
+    email: string;
+    phone: string;
+    picture?: Picture;
+    location?: Location;
+  }
+  interface Picture {
+    large: string;
+    medium: string;
+    thumbnail: string;
+  }
+  interface Name {
+    first: string
+    last: string
+    title: string
+  }
+  interface Location{ 
+    city: string
+    country: string
+    postcode: number
+    state: string
+    street: Street
+  }
+  interface Street{
+    number: number
+    name: string
+  }
+  const [seed, setSeed] = useState("");
+  const url: string = `https://randomuser.me/api/?seed=${seed}`; //seed value is from inputbox
+  const data:UserData = useFetch(url); //the url relies on seed input
+  const defVal: Results = {
+    name: {
+      first: "",
+      last: "",
+      title: "",
+    },
+    email : "" , 
+    phone : "",
+    picture: {
+      large: "",
+      medium: "",
+      thumbnail: ""
+    },
+    location: {
+      city:"",
+      country: "",
+      postcode: 69,
+      state: "",
+      street: {
+        number: 66,
+        name: ""
+      }
+    }
+  }
+
   return (
     <div className="container">
-      <InputForm setSeed={setSeed}/>
-      {seed==='' ? <Welcome /> : <OutputCard usersData={data} />}
+      <InputForm setSeed={setSeed} />
+      {seed === "" ? <Welcome /> : <OutputCard usersData={data.results? data.results[0] : defVal} />}
     </div>
   );
-}
+};
 
 export default App;
--- src/OutputCard.tsx
+++ src/OutputCard.tsx
@@ -1,51 +1,60 @@
 interface Prop{
-    usersData: {
-      results?: Array<Results>
-      info?: unknown
-    }
+  usersData: Result
 }
-
-interface Results {
-      name?: unknown
-      email: string
-      phone: string
-      picture?: Picture
-      location?: unknown
+interface Result {
+  name?: Name;
+  email: string;
+  phone: string;
+  picture?: Picture;
+  location?: Location;
 }
-
 interface Picture {
-  large: string
-  medium : string
-  thumbnail:string
+  large: string;
+  medium: string;
+  thumbnail: string;
 }
-
-
-
+interface Name {
+  first: string
+  last: string
+  title: string
+}
+interface Location{ 
+  city: string
+  country: string
+  postcode: number
+  state: string
+  street: Street
+}
+interface Street{
+  number: number
+  name: string
+}
 export const OutputCard = ({usersData}:Prop) =>{
-    console.log("Results", usersData.results? usersData.results[0] : {}) //Error when I start to add the index
+    console.log("Outputbox prop", usersData) //Error when I start to add the index
     return (
         <div className="below-input-box">
           <figure className="img-prof">
             <img alt="USER-PROFILE"
-            src = {usersData.results? usersData.results[0].picture?.large : ""} />
+            src = {usersData?.picture?.large} 
+            />
           </figure>
 
           <div className="user-info-card">
               <div className="user-name user-details head-text">
                 <i className="fas fa-user"></i>
-                {/* {` ${u.name.title} ${u.name.first} ${u.name.last}`} */}
+                {` ${usersData?.name?.title} ${usersData?.name?.first} ${usersData?.name?.last}`}
               </div>
               <div className="user-details head-text">
                 <i className="fas fa-map-marker-alt"></i>
-                {/* {` ${u.location.street.name} ${u.location.street.number}, ${u.location.city}, ${u.location.state} ${u.location.postcode} ${u.location.country}`} */}
+                {` ${usersData?.location?.street.name} ${usersData?.location?.street.number}, ${usersData?.location?.city}, ${usersData?.location?.state} ${usersData?.location?.postcode} ${usersData?.location?.country}`}
               </div>
               <div className="user-details head-text">
                 <i className="fas fa-envelope"></i>
-                {/* {` ${u.email}`} */}
+                {` ${usersData.email}`}
               </div>
               <div className="user-details head-text">
                 <i className="fas fa-phone-alt"></i>
-                {/* {` ${u.phone}`} */}
+                {` ${usersData.phone}`}
               </div>
             </div>
         </div>
