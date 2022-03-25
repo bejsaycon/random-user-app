b5721ac1e5ec73e9aeb0f1608d789cc44f574c79
Bryan Jeff Saycon <bryanjeff.saycon@g.msuiit.edu.ph>
2022-03-25 22:18:51 +0800

    Another interface added. But still wont run correctly

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,15 @@ import { Welcome } from './Welcome';
 import { useFetch } from './useFetch';
 import { OutputCard } from './OutputCard';
 
-
 const App = () => {
   const [seed, setSeed] = useState('');
   const url:string = `https://randomuser.me/api/?seed=${seed}`; //seed value is from inputbox
-  let usersData = useFetch(url); //the url relies on seed input
-  console.log(typeof(usersData));
+  const data = useFetch(url); //the url relies on seed input
   return (
     <div className="container">
       <InputForm setSeed={setSeed}/>
-      {seed==='' ? <Welcome /> : <OutputCard usersData={usersData as object}/>}
+      {seed==='' ? <Welcome /> : <OutputCard usersData={data} />}
     </div>
-    
   );
 }
 
