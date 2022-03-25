813534240d6e6a173d2723a84137163ea7ec96fd
Bryan Jeff Saycon <bryanjeff.saycon@g.msuiit.edu.ph>
2022-03-26 00:18:53 +0800

    fixed issue of array of objects error

--- src/InputForm.tsx
+++ src/InputForm.tsx
@@ -3,14 +3,11 @@ import React from "react";
 interface SetState {
     setSeed: React.Dispatch<React.SetStateAction<string>>
 }
-
 export const InputForm = ({setSeed}:SetState) => {
-
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     setSeed((event.currentTarget[0] as HTMLInputElement).value);
   };    
-
   return (
     <form className="form-container" onSubmit={handleSubmit}>
       <input className="input" placeholder="Enter value" name="seed" required />
